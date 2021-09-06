14529dd0d05e7dafe97a52c4173050c1a130f890
Leonardo Valle <59975691+painlenk@users.noreply.github.com>
2021-09-06 15:32:30 -0300

    final

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas =[
+    {
+        titulo:'Governador',
+        numeros:4,
+        candidatos: [
+            {
+                numero:'2424',
+                nome:'Ciro',
+                partido:'Cangaço',
+                fotos:[
+                    {url:"./assets/ciro.jpg", legenda:'Governador', small:false}
+                ]
+            },
+            {
+                numero:'6666',
+                nome:'Bolsonaro',
+                partido:'NemSei',
+                fotos:[
+                    {url:'./assets/bolsonaro.jpg', legenda:'Governador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo:'Presidente',
+        numeros:2,
+        candidatos: [
+            {
+                numero:'99',
+                nome:'Lula',
+                partido:'Ptralha',
+                vice:'Dilma',
+                fotos:[
+                    {url:'./assets/lula.jpg', legenda:'Presidente'},
+                    {url:'./assets/dilma.jpg', legenda:'Vice-Presidente', small:true}
+                ]
+            },
+            {
+                numero:'98',
+                nome:'Trump',
+                partido:'PewPew',
+                vice:'Obama',
+                fotos:[
+                    {url:'./assets/trump.jpg', legenda: 'Prefeito'},
+                    {url:'./assets/obama.jpg', legenda:'Vice-Prefeito', small:true}
+                ]
+            },
+        ]
+    }
+];
